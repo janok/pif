25714568fdb4bd714e112ba2c009ceaeddc2ca79
mattias <lundmark.mattias@gmail.com>
2014-10-17 17:58:12 +0200

    Centering map and setting marker

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -1,13 +1,27 @@
 /**
  *
  */
-/*$(document).ready(function() {
+$(document).ready(function() {
     $.ajax({
         url: "http://bouvet-code-camp.azurewebsites.net/api/game/pif/hentmeldinger/55"
     }).then(function(data) {
         $('.navn').append(data.navn);
     });
-});*/
+    var map = initializeMap();
+    setMyPositionMarker(map);
+});
+
+function initializeMap() {
+    
+    var mapOptions = {
+        center: { lat: 60, lng: 9},
+        zoom: 16
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    setCurrentPosition(map);
+    return map;
+}
+
 
 function setCurrentPosition(map) {
     if (navigator.geolocation) {
@@ -16,6 +30,24 @@ function setCurrentPosition(map) {
             map.setCenter(initialLocation);
         });
     }
+}
+
+function setMyPositionMarker(map) {
+   
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title:"Kor i svartre er eg?"
+            });
+
+            
+            marker.setMap(map);
+            map.setCenter(myLocation);
+        });
+    }
+    
 }
 
 
